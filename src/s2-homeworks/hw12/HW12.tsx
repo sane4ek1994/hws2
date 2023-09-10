d2b8d83dbaaaa7a16c0fd90ca6c1d67f1cb9706b
Alex Naumov <mfq07k9cz@gmail.com>
2023-09-10 21:28:34 +0300

    fix delpoy HW12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -43,7 +43,7 @@ const HW12 = () => {
                     className={s.select}
                     // сделать переключение тем
                     options={themes}
-                    value={themeId}
+                    value={String(themeId)}
                     onChangeOption={change}
                 />
             </div>
