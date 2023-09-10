d2f795f1223921438824fe99566bf2e2fbf892c0
Alex Naumov <mfq07k9cz@gmail.com>
2023-09-10 21:21:08 +0300

    HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,29 @@
-const initState = {
-    themeId: 1,
+export enum THEME_VALUE {
+    LIGHT = 1,
+    BLUE = 2,
+    DARK = 3
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type TThemeState = {
+    themeId: THEME_VALUE
+}
+const initState = {
+    themeId: THEME_VALUE.LIGHT,
+}
+
+type TActions = ReturnType<typeof changeThemeId>
+
+export const themeReducer = (state: TThemeState = initState, action: TActions): TThemeState => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: THEME_VALUE) => ({type: 'SET_THEME_ID', id} as const) // fix any
