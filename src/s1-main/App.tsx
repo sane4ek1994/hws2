8d62cf79d2e6a922597fc1790258f6af52d761c3
Alex Naumov <mfq07k9cz@gmail.com>
2023-09-24 14:42:18 +0300

    HW15

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -5,14 +5,7 @@ import s from './App.module.css'
 // import HW3 from "../s2-homeworks/hw03/HW3"
 // import HW4 from "../s2-homeworks/hw04/HW4"
 import HW5 from "../s2-homeworks/hw05/HW5";
-import HW6 from "../s2-homeworks/hw06/HW6";
-import HW7 from "../s2-homeworks/hw07/HW7";
-import HW8 from "../s2-homeworks/hw08/HW8";
-import HW9 from "../s2-homeworks/hw09/HW9";
-import HW10 from "../s2-homeworks/hw10/HW10";
-import HW11 from "../s2-homeworks/hw11/HW11";
-import HW12 from "../s2-homeworks/hw12/HW12";
-import HW13 from "../s2-homeworks/hw13/HW13";
+
 
 function App() {
     return (
@@ -25,14 +18,6 @@ function App() {
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5/>
-            <HW6/>
-            <HW7/>
-            <HW8/>
-            <HW9/>
-            <HW10/>
-            <HW11/>
-            <HW12/>
-            <HW13/>
         </div>
     )
 }
